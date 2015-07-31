e15c5bcb2bb502bf0c17b700b365215ddfde5421
Penny Nguyen <i_throw_paper@yahoo.com>
2015-07-31 11:20:44 -0700

    down works, but working on up button(testing github username)

--- src/screen/TitleScreen.java
+++ src/screen/TitleScreen.java
@@ -98,16 +98,7 @@ public class TitleScreen extends Screen {
 	 * @return The previous menu item
 	 */
 	private ScreenType getPreviousMenuItem(ScreenType selectedMenuItem) {
-		if (selectedMenuItem == ScreenType.GameScreen) {
-			return ScreenType.HighScoreScreen;
-		}
-		else if (selectedMenuItem == ScreenType.HighScoreScreen) {
-			return ScreenType.EndGame;
-		}
-		else {
-			return ScreenType.GameScreen;
-		}
-	}
+		
 
 	/**
 	 * Draws the elements associated with the screen.
