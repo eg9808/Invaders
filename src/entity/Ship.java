bafaf6713efaeee42340b6f0e13aa4d240f4526b
testrcjames <testrcjames@gmail.com>
2015-08-05 11:42:25 -0700

    Issue 4 complete

--- src/entity/Ship.java
+++ src/entity/Ship.java
@@ -55,6 +55,13 @@ public class Ship extends Entity {
 
 		// Detects whether the ship is not touching the left border
 		boolean isShipNotTouchingLeftBorder = !(this.getPositionX() - this.getSpeed() < 1);
+
+		if (Main.getInputManager().isRightKeyDown() && isShipNotTouchingRightBorder) {
+			this.moveRight();
+		}
+		else if (Main.getInputManager().isLeftKeyDown() && isShipNotTouchingLeftBorder) {
+			this.moveLeft();
+		}
 	}
 
 	/**
